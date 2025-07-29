257aacdcbab265da7ec2e01fa3c1f17ed0e2be4c
kiran057-clould <kiranvs057@gmail.com>
2025-07-29 11:30:51 +0530

    Add files via upload

---
+++ mukesh.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter first integer number: "))
+num2=int(input("Enter second integer number: "))
+sum=num1+num2
+print("Addition of 2 integer number is:",sum)
+
+str1=str(input("Enter first string: "))
+str2=str(input("Enter second string: "))
+str3=str1+str2
+print("Addition of 2 string is:", str3)
